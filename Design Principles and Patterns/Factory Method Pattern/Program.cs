b67e947be08c5fe805065ce3cc3eeef8822e16bd
j4ik2i5x0 <jaikris24@gmail.com>
2025-06-18 22:33:50 +0530

    Week 1 Assignment

---
+++ Design Principles and Patterns/Factory Method Pattern/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public interface IDocument
+{
+    void Open();
+}
+
+public class WordDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening Word Document...");
+    }
+}
+
+public class PdfDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening PDF Document...");
+    }
+}
+
+public class ExcelDocument : IDocument
+{
+    public void Open()
+    {
+        Console.WriteLine("Opening Excel Document...");
+    }
+}
+
+public abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+
+public class WordFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new WordDocument();
+    }
+}
+
+public class PdfFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
+
+public class ExcelFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new ExcelDocument();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        DocumentFactory wordDocFactory = new WordFactory();
+        IDocument wordDoc = wordDocFactory.CreateDocument();
+        wordDoc.Open();
+
+        DocumentFactory pdfDocFactory = new PdfFactory();
+        IDocument pdfDoc = pdfDocFactory.CreateDocument();
+        pdfDoc.Open();
+
+        DocumentFactory excelDocFactory = new ExcelFactory();
+        IDocument excelDoc = excelDocFactory.CreateDocument();
+        excelDoc.Open();
+    }
+}
---
+++ Design Principles and Patterns/Singleton Pattern/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Logger
+{
+    private static Logger? _instance = null;
+
+    private Logger()
+    {
+        Console.WriteLine("Logger instance created.");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Logger();
+        }
+        return _instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("Log Message: " + message);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Logger logger1 = Logger.GetInstance();
+        logger1.Log("This is the first message.");
+
+        Logger logger2 = Logger.GetInstance();
+        logger2.Log("This is the second message.");
+
+        if (logger1 == logger2)
+        {
+            Console.WriteLine("Both logger1 and logger2 refer to the same instance.");
+        }
+    }
+}
