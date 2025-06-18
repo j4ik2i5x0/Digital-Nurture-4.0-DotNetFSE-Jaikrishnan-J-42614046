8461f43071f37153948515d4fa9ad893972468a8
j4ik2i5x0 <jaikris24@gmail.com>
2025-06-18 22:41:18 +0530

    Output.png updated

--- Design Principles and Patterns/Factory Method Pattern/obj/Debug/net9.0/Factory Method Pattern.AssemblyInfo.cs
+++ Design Principles and Patterns/Factory Method Pattern/obj/Debug/net9.0/Factory Method Pattern.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Factory Method Pattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09a372069814bdf0ee2597fd390bf06b8ecbee28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Factory Method Pattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Factory Method Pattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
